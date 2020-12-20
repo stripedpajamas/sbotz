d62e396091f26ee5fa92a262c4e81201aacf1b7d
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-12-20 00:08:50 -0800

    Client hello impl

---
+++ main.zig
@@ -0,0 +1,40 @@
+const std = @import("std");
+const shs = @import("./shs.zig");
+
+const net = std.net;
+const HandshakeClient = shs.HandshakeClient;
+
+const network_id = [_]u8{
+    0xd4, 0xa1, 0xcb, 0x88, 0xa6, 0x6f, 0x02, 0xf8,
+    0xdb, 0x63, 0x5c, 0xe2, 0x64, 0x41, 0xcc, 0x5d,
+    0xac, 0x1b, 0x08, 0x42, 0x0c, 0xea, 0xac, 0x23,
+    0x08, 0x39, 0xb7, 0x55, 0x84, 0x5a, 0x9f, 0xfb,
+};
+
+pub fn main() !void {
+    var allocator = std.testing.allocator;
+    var keypair = try shs.loadKeypair(allocator);
+
+    var client = HandshakeClient.init(allocator, .{
+        .keypair = keypair.?,
+        .network_id = network_id,
+    });
+
+    var server_addr = try net.Address.parseIp4("127.0.0.1", 8008);
+    var conn = try net.tcpConnectToAddress(server_addr);
+
+    var writer = conn.writer();
+    var reader = conn.reader();
+
+    var session = try client.newSession();
+
+    var hello_msg = try session.hello();
+    var written = try writer.write(hello_msg);
+
+    std.log.info("{} bytes written\n", .{written});
+
+    var server_hello = try allocator.alloc(u8, 64);
+    try reader.readNoEof(server_hello);
+
+    std.log.info("response: {x}\n", .{server_hello});
+}
--- shs.zig
+++ shs.zig
@@ -1,13 +1,15 @@
 const std = @import("std");
 const mem = std.mem;
+const crypto = std.crypto;
+const Hmac = crypto.auth.hmac.sha2.HmacSha512;
 
-const Keypair = struct {
+const ScuttlebuttKey = struct {
     curve: []const u8,
     id: []const u8,
     public: []const u8,
     private: []const u8,
 
-    pub fn parseKeyFile(allocator: *mem.Allocator, keyfile: []const u8) !Keypair {
+    pub fn parseKeyFile(allocator: *mem.Allocator, keyfile: []const u8) !ScuttlebuttKey {
         // remove all the bs comment lines (start with #)
         var clean_file: [384]u8 = undefined;
 
@@ -34,7 +36,7 @@ const Keypair = struct {
 
         // json parse the result
         var json_stream = std.json.TokenStream.init(clean_file[0..cf_idx]);
-        const keypair = try std.json.parse(Keypair, &json_stream, .{
+        const keypair = try std.json.parse(ScuttlebuttKey, &json_stream, .{
             .allocator = allocator,
         });
 
@@ -42,7 +44,7 @@ const Keypair = struct {
     }
 };
 
-fn loadKeypair(allocator: *mem.Allocator) !?Keypair {
+pub fn loadKeypair(allocator: *mem.Allocator) !?ScuttlebuttKey {
     const home_dir = if (std.os.getenv("HOME")) |home|
         home
     else
@@ -59,15 +61,62 @@ fn loadKeypair(allocator: *mem.Allocator) !?Keypair {
     const dir = try std.fs.cwd().openDir(path, .{});
     const keyfile = try dir.readFileAlloc(allocator, "secret", 1024);
 
-    const keypair = try Keypair.parseKeyFile(allocator, keyfile);
+    const keypair = try ScuttlebuttKey.parseKeyFile(allocator, keyfile);
 
     return keypair;
 }
 
-pub fn ssbKeys(allocator: *mem.Allocator) ?[]const u8 {}
+pub const HandshakeOptions = struct {
+    keypair: ScuttlebuttKey,
+    network_id: [32]u8,
+};
 
-pub fn main() !void {
-    var allocator = std.testing.allocator;
-    var keypair = (try loadKeypair(allocator)) orelse unreachable;
-    std.log.info("{}\n", .{keypair});
-}
+pub const HandshakeClient = struct {
+    allocator: *mem.Allocator,
+    opts: HandshakeOptions,
+
+    pub fn init(allocator: *mem.Allocator, opts: HandshakeOptions) HandshakeClient {
+        return HandshakeClient{
+            .allocator = allocator,
+            .opts = opts,
+        };
+    }
+
+    const Session = struct {
+        client: *HandshakeClient,
+        eph_keypair: crypto.nacl.SealedBox.KeyPair,
+
+        pub const hello_len: usize = 64;
+
+        pub fn hello(session: *Session) ![]const u8 {
+            // concat(
+            //   nacl_auth(
+            //     msg: client_ephemeral_pk,
+            //     key: network_identifier
+            //   ),
+            //   client_ephemeral_pk
+            // )
+            var out = try session.client.allocator.alloc(u8, hello_len);
+            errdefer session.client.allocator.free(out);
+
+            Hmac.create(out[0..Hmac.mac_length], &session.eph_keypair.public_key, &session.client.opts.network_id);
+            mem.copy(u8, out[out.len - session.eph_keypair.public_key.len ..], &session.eph_keypair.public_key);
+
+            return out;
+        }
+    };
+
+    pub fn newSession(self: *HandshakeClient) !Session {
+        // generate ephemeral keys for this session
+        var eph_keypair = try crypto.nacl.SealedBox.KeyPair.create(null);
+
+        return Session{
+            .client = self,
+            .eph_keypair = eph_keypair,
+        };
+    }
+
+    pub fn whoami(self: *HandshakeClient) []const u8 {
+        return self.opts.keypair.public;
+    }
+};
