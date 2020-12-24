12936300539488606b727c7141c17f0faf6d3fcc
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-12-24 15:01:07 -0800

    Buffering box stuff now

--- main.zig
+++ main.zig
@@ -13,48 +13,67 @@ pub fn main() !void {
     var allocator = std.testing.allocator;
     var keyfile = try keys.loadKeyfile(allocator);
 
-    const opts = shs.HandshakeOptions{
-        .keypair = keyfile.keypair,
-        .network_id = keys.network_id,
-    };
-
     var server_addr = try net.Address.parseIp4("127.0.0.1", 8008);
     var conn = try net.tcpConnectToAddress(server_addr);
 
-    var rpc_conn = try RPCConnection.init(opts, conn, keyfile.keypair.public_key);
+    var boxed_conn = try box.BoxedConnection.init(.{
+        .keypair = keyfile.keypair,
+        .network_id = keys.network_id,
+        .remote_pk = keyfile.keypair.public_key,
+    }, conn);
 
-    var whoami_req = try RPCConnection.whoami(allocator);
-    var req = try RPCConnection.encode(allocator, whoami_req, 100, .{
-        .stream = false,
-        .end_err = false,
-        .body_type = RPCConnection.BodyType.JSON,
+    var rpc_conn = RPCConnection(box.BoxedConnection).init(allocator, &boxed_conn);
+
+    var whoami = "{\"name\":[\"whoami\"],\"type\":\"sync\",\"args\":[]}";
+    _ = try rpc_conn.writeMessage(rpc.Message{
+        .header = rpc.Header{
+            .flags = .{
+                .stream = false,
+                .end_err = false,
+                .body_type = rpc.Header.BodyType.JSON,
+            },
+            .body_len = whoami.len,
+            .req_num = 1,
+        },
+        .body = whoami[0..],
     });
 
-    try rpc_conn.write(req);
-
-    var response: [4096]u8 = undefined;
-    var res_size = try rpc_conn.boxed_conn.readNextBox(&response);
-
-    std.log.info("received {} byte box", .{res_size});
-
-    if (res_size) |sz| {
-        var res = response[0..sz];
-        std.log.info("total size: {}", .{res.len});
-        std.log.info("flags: {x}", .{res[0]});
-        std.log.info("body len: {}", .{std.mem.readIntBig(u32, res[1..5])});
-        std.log.info("req num: {}", .{std.mem.readIntBig(i32, res[5..9])});
+    if (try rpc_conn.readNextMessage()) |msg| {
+        std.log.info("msg: {}", .{msg.body});
     }
 
-    res_size = try rpc_conn.boxed_conn.readNextBox(&response);
+    // var whoami_req = try RPCConnection.whoami(allocator);
+    // var req = try RPCConnection.encode(allocator, whoami_req, 100, .{
+    //     .stream = false,
+    //     .end_err = false,
+    //     .body_type = RPCConnection.BodyType.JSON,
+    // });
 
-    std.log.info("received {} byte box", .{res_size});
+    // try rpc_conn.write(req);
 
-    if (res_size) |sz| {
-        var res = response[0..sz];
-        std.log.info("total size: {}", .{res.len});
-        std.log.info("is it dope? {}", .{res});
-        // std.log.info("flags: {x}", .{res[0]});
-        // std.log.info("body len: {x}", .{res[1..5]});
-        // std.log.info("req num: {x}", .{res[5..9]});
-    }
+    // var response: [4096]u8 = undefined;
+    // var res_size = try rpc_conn.boxed_conn.readNextBox(&response);
+
+    // std.log.info("received {} byte box", .{res_size});
+
+    // if (res_size) |sz| {
+    //     var res = response[0..sz];
+    //     std.log.info("total size: {}", .{res.len});
+    //     std.log.info("flags: {x}", .{res[0]});
+    //     std.log.info("body len: {}", .{std.mem.readIntBig(u32, res[1..5])});
+    //     std.log.info("req num: {}", .{std.mem.readIntBig(i32, res[5..9])});
+    // }
+
+    // res_size = try rpc_conn.boxed_conn.readNextBox(&response);
+
+    // std.log.info("received {} byte box", .{res_size});
+
+    // if (res_size) |sz| {
+    //     var res = response[0..sz];
+    //     std.log.info("total size: {}", .{res.len});
+    //     std.log.info("is it dope? {}", .{res});
+    //     // std.log.info("flags: {x}", .{res[0]});
+    //     // std.log.info("body len: {x}", .{res[1..5]});
+    //     // std.log.info("req num: {x}", .{res[5..9]});
+    // }
 }
--- rpc.zig
+++ rpc.zig
@@ -5,52 +5,156 @@ const mem = std.mem;
 
 const log = std.log.scoped(.rpc);
 
+pub const header_size: usize = 9;
+
+pub const Header = struct {
+    pub const BodyType = enum {
+        Binary,
+        String,
+        JSON,
+    };
+
+    pub const Flags = struct {
+        stream: bool,
+        end_err: bool,
+        body_type: BodyType,
+    };
+
+    flags: Flags,
+    body_len: usize,
+    req_num: i32,
+
+    pub fn init(header: [header_size]u8) Header {
+        var flags = Flags{
+            .stream = header[0] & 8 == 8,
+            .end_err = header[0] & 4 == 4,
+            .body_type = switch (header[0] & 3) {
+                0 => BodyType.Binary,
+                1 => BodyType.String,
+                2 => BodyType.JSON,
+                else => unreachable,
+            },
+        };
+
+        var body_len = @as(usize, mem.readIntBig(u32, header[1..5]));
+        var req_num = mem.readIntBig(i32, header[5..9]);
+
+        return Header{
+            .flags = flags,
+            .body_len = body_len,
+            .req_num = req_num,
+        };
+    }
+
+    pub fn serialize(header: Header, out: *[header_size]u8) void {
+        var flag_byte: u8 = 0;
+        if (header.flags.stream) {
+            flag_byte |= 0b1000;
+        }
+        if (header.flags.end_err) {
+            flag_byte |= 0b0100;
+        }
+        switch (header.flags.body_type) {
+            BodyType.String => {
+                flag_byte |= 0b0001;
+            },
+            BodyType.JSON => {
+                flag_byte |= 0b0010;
+            },
+            BodyType.Binary => {},
+        }
+
+        out[0] = flag_byte;
+
+        // next 4 bytes are body length as a 32-bit BE uint
+        mem.writeIntBig(u32, out[1..5], @truncate(u32, header.body_len));
+        // next 4 bytes are request num as a 32-bit BE int
+        mem.writeIntBig(i32, out[5..9], header.req_num); // write the request number
+    }
+};
+
+pub const Message = struct {
+    header: Header,
+    body: []const u8,
+
+    // out must be body.len + header_size
+    pub fn serialize(msg: Message, out: []u8) void {
+        std.debug.assert(out.len == body.len + header_size);
+
+        Header.serialize(out[0..header_size]);
+        mem.copy(u8, out[header_size..], body);
+    }
+};
+
 pub fn RPCConnection(comptime ReaderWriterType: type) type {
     return struct {
-        buffered_reader: std.io.BufferedReader(4130, ReaderWriterType),
+        // underlying file/connection
+        conn: *ReaderWriterType,
+
+        allocator: *mem.Allocator,
 
         const Self = @This();
 
-        pub fn init(conn: ReaderWriterType) Self {
-            var buffered_reader = std.io.BufferedReader(4130, ReaderWriterType){
-                .unbuffered_reader = conn,
-            };
-
+        pub fn init(allocator: *mem.Allocator, conn: *ReaderWriterType) Self {
             return Self{
-                .buffered_reader = buffered_reader,
+                .allocator = allocator,
+                .conn = conn,
             };
         }
 
-        pub fn readNextMessage(self: *Self, out: []u8) !usize {
-            return self.buffered_reader.read(out);
+        // reads out next header, or returns null if there isn't one
+        pub fn readNextHeader(self: Self) !?Header {
+            var header: [header_size]u8 = undefined;
+            const n = try self.conn.read(&header);
+
+            if (n != header_size) {
+                return null;
+            }
+
+            var msg_header = Header.init(header);
+
+            return msg_header;
+        }
+
+        // caller owns returned body slice
+        pub fn readNextMessage(self: *Self) !?Message {
+            var header = try self.readNextHeader();
+
+            if (header) |h| {
+                log.info("opened rpc header; expecting {} byte msg", .{h.body_len});
+                var out = try self.allocator.alloc(u8, h.body_len);
+
+                const n = try self.conn.read(out);
+
+                if (n != out.len) {
+                    return error.EndOfStream;
+                }
+
+                return Message{
+                    .header = h,
+                    .body = out,
+                };
+            } else {
+                // there is no next header; underlying source has dried up
+                return null;
+            }
+        }
+
+        pub fn writeMessage(self: *Self, msg: Message) !usize {
+            var payload = std.ArrayList(u8).init(self.allocator);
+            defer payload.deinit();
+
+            var writer = payload.writer();
+
+            var header: [header_size]u8 = undefined;
+            msg.header.serialize(&header);
+            try writer.writeAll(&header);
+
+            try writer.writeAll(msg.body);
+
+            return self.conn.write(payload.items);
         }
     };
-    // boxed_conn: BoxedConnection,
-
-    // pub const header_size: usize = 9;
-
-    // pub const BodyType = enum {
-    //     Binary,
-    //     String,
-    //     JSON,
-    // };
-
-    // pub const Flags = struct {
-    //     stream: bool,
-    //     end_err: bool,
-    //     body_type: BodyType,
-    // };
-
-    // pub fn init(conn: fs.File) RPCConnection {
-    //     return RPCConnection{
-    //         .boxed_conn = boxed_conn,
-    //     };
-    // }
-
-    // pub fn write(self: *RPCConnection, payload: []const u8) !void {
-    //     try self.boxed_conn.write(payload);
-    // }
-
     // // adds the rpc header to the cmd payload
     // pub fn encode(allocator: *mem.Allocator, cmd: []const u8, req_num: i32, flags: Flags) ![]const u8 {
     //     var out = try allocator.alloc(u8, cmd.len + RPCConnection.header_size);
@@ -94,18 +198,4 @@ pub fn RPCConnection(comptime ReaderWriterType: type) type {
 
     //     return str.toOwnedSlice();
     // }
-}
-
-test "rpc wraps a connection" {
-    var allocator = std.testing.allocator;
-
-    var file = try fs.cwd().openFile("rpc.zig", .{ .read = true });
-
-    var rpc = RPCConnection(fs.File.Reader).init(file.reader());
-
-    var msg: [30]u8 = undefined;
-    var count = try rpc.readNextMessage(&msg);
-
-    std.log.warn("count: {}", .{count});
-    std.log.warn("{}", .{msg});
 }
